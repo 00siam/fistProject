a6471c30f5d5cbc0a471983a35e3dfbc3b7737a6
Siam <baust.cse.200201018@gmail.com>
2022-10-17 22:42:07 +0600

    work done
    all work done

---
+++ myFirstProject/Data/myFirstProjectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using myFirstProject.Models;
+
+namespace myFirstProject.Data
+{
+    public class myFirstProjectContext : DbContext
+    {
+        public myFirstProjectContext (DbContextOptions<myFirstProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<myFirstProject.Models.Student> Student { get; set; } = default!;
+    }
+}
